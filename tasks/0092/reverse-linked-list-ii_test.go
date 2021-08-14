93e71290da24a8f638a8975e1cd4c7067fbe052a
hidypeople <hidy.people@gmail.com>
2021-08-14 14:15:18 +0500

    Solved #92

---
+++ tasks/0092/reverse-linked-list-ii_test.go
@@ -0,0 +1,42 @@
+package tasks
+
+import (
+	. "leetcode/linkedList"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverseBetween(t *testing.T) {
+	assert.Nil(t, ReverseBetween(nil, 1, 2))
+}
+
+func TestReverseBetween2(t *testing.T) {
+	list := LinkedListFromArray([]int{1, 2, 3, 4, 5, 6, 7})
+	result := LinkedListToArray(ReverseBetween(list, 2, 4))
+	assert.Equal(t, []int{1, 4, 3, 2, 5, 6, 7}, result)
+}
+
+func TestReverseBetween3(t *testing.T) {
+	list := LinkedListFromArray([]int{4})
+	result := LinkedListToArray(ReverseBetween(list, 1, 1))
+	assert.Equal(t, []int{4}, result)
+}
+
+func TestReverseBetween4(t *testing.T) {
+	list := LinkedListFromArray([]int{1, 2, 3, 4, 5, 6, 7})
+	result := LinkedListToArray(ReverseBetween(list, 1, 7))
+	assert.Equal(t, []int{7, 6, 5, 4, 3, 2, 1}, result)
+}
+
+func TestReverseBetween5(t *testing.T) {
+	list := LinkedListFromArray([]int{1, 2, 3, 4, 5, 6, 7})
+	result := LinkedListToArray(ReverseBetween(list, 1, 1))
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7}, result)
+}
+
+func TestReverseBetween6(t *testing.T) {
+	list := LinkedListFromArray([]int{1, 2, 3, 4, 5, 6, 7})
+	result := LinkedListToArray(ReverseBetween(list, 7, 7))
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7}, result)
+}
