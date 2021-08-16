695732374ff4b05e1ff4def6e6b2eb7436ed5d09
hidypeople <hidy.people@gmail.com>
2021-08-17 00:30:20 +0500

    Solved #108

---
+++ tasks/task0108/convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,65 @@
+package task0108
+
+import . "leetcode/binaryTree"
+
+type Slice struct {
+	l    int
+	r    int
+	node *TreeNode
+}
+
+// Given an integer array nums where the elements are sorted in ascending order,
+// convert it to a height-balanced binary search tree. A height-balanced binary
+// tree is a binary tree in which the depth of the two subtrees of every node
+// never differs by more than one.
+// Constraints:
+//   1 <= nums.length <= 104
+//   -104 <= nums[i] <= 104
+//   nums is sorted in a strictly increasing order.
+func SortedArrayToBST(nums []int) *TreeNode {
+	// We'll solve it using queue (also it is possible to use recursive function calls)
+	slice := Slice{
+		0,
+		len(nums) - 1,
+		&TreeNode{
+			Val:   0,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+	rootNode := slice.node
+
+	queue := []Slice{slice}
+	for len(queue) > 0 {
+		// dequeue
+		currentSlice := queue[0]
+		queue = queue[1:]
+
+		// calc middle (always exists)
+		middle := (currentSlice.r-currentSlice.l)/2 + currentSlice.l
+
+		// Set up curr val
+		currentSlice.node.Val = nums[middle]
+
+		// Enqueue left and right parts
+		if currentSlice.l < middle {
+			leftNode := &TreeNode{
+				Val:   0,
+				Left:  nil,
+				Right: nil,
+			}
+			currentSlice.node.Left = leftNode
+			queue = append(queue, Slice{currentSlice.l, middle - 1, leftNode})
+		}
+		if middle < currentSlice.r {
+			rightNode := &TreeNode{
+				Val:   0,
+				Left:  nil,
+				Right: nil,
+			}
+			currentSlice.node.Right = rightNode
+			queue = append(queue, Slice{middle + 1, currentSlice.r, rightNode})
+		}
+	}
+	return rootNode
+}
---
+++ tasks/task0108/convert-sorted-array-to-binary-search-tree_test.go
@@ -0,0 +1,36 @@
+package task0108
+
+import (
+	. "leetcode/binaryTree"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSortedArrayToBST(t *testing.T) {
+	expected := BSTFromArray([]*int{
+		I(0),
+		I(-2), I(2),
+		I(-3), I(-1), I(1), I(3),
+	})
+	result := SortedArrayToBST([]int{-3, -2, -1, 0, 1, 2, 3})
+	assert.Equal(t, expected, result)
+}
+
+func TestSortedArrayToBST2(t *testing.T) {
+	expected := BSTFromArray([]*int{
+		I(-1),
+		I(-3), I(2),
+		nil, I(-2), I(1), I(3),
+	})
+	result := SortedArrayToBST([]int{-3, -2, -1, 1, 2, 3})
+	assert.Equal(t, expected, result)
+}
+
+func TestSortedArrayToBST3(t *testing.T) {
+	expected := BSTFromArray([]*int{
+		I(-1),
+	})
+	result := SortedArrayToBST([]int{-1})
+	assert.Equal(t, expected, result)
+}
