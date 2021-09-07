ed2b08d243d57ffc3fabf5e3c51e1806eb681811
hidypeople <hidy.people@gmail.com>
2021-09-08 00:52:01 +0500

    Solved #885

---
+++ tasks/task0885/spiral-matrix-iii_test.go
@@ -0,0 +1,18 @@
+package task0885
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_spiralMatrixIII(t *testing.T) {
+	assert.Equal(t, [][]int{{0, 0}, {0, 1}, {0, 2}, {0, 3}}, spiralMatrixIII(1, 4, 0, 0))
+}
+
+func Test_spiralMatrixIII2(t *testing.T) {
+	assert.Equal(t,
+		[][]int{{1, 4}, {1, 5}, {2, 5}, {2, 4}, {2, 3}, {1, 3}, {0, 3}, {0, 4}, {0, 5}, {3, 5}, {3, 4}, {3, 3}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {4, 5}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
+		spiralMatrixIII(5, 6, 1, 4),
+	)
+}
