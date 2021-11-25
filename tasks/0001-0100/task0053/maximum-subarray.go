263642bf117ae3d1ae20c00110d3ec1ad5d8e940
Alex Yarmolchuk <alex.yarmolchuk@logianalytics.com>
2021-11-25 11:05:38 +0500

    #53 Updated

--- tasks/0001-0100/task0053/maximum-subarray.go
+++ tasks/0001-0100/task0053/maximum-subarray.go
@@ -9,8 +9,8 @@ package task0053
 //  -10^5 <= nums[i] <= 10^5
 //
 // Result:
-//   Runtime: 4 ms, faster than 94.12% of Go online submissions for Maximum Subarray.
-//   Memory Usage: 3.2 MB, less than 38.74% of Go online submissions for Maximum Subarray.
+//   Runtime: 92 ms, faster than 97.60% of Go online submissions for Maximum Subarray.
+//   Memory Usage: 9.5 MB, less than 27.12% of Go online submissions for Maximum Subarray.
 func maxSubArray(nums []int) int {
 	maxSub := nums[0]
 	currSum := 0
